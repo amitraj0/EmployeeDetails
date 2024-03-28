9b9fbcf47dfabca14497c420ed57a094af8cac00
raj879720@gmail.com <Theflash@1>
2024-03-28 22:54:50 +0530

    controller added

---
+++ EmpApp/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDetails() { 
+        return Ok( "Employee=1");
+        }
+    }
+}
